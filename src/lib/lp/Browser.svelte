f3cfa5dddc3e69d958d8cc8c9d70d7c27f8d11d8
esaaracaa <shariasaba@gmail.com>
2023-11-26 00:14:18 +0400

    11

---
+++ src/lib/lp/Browser.svelte
@@ -0,0 +1,16 @@
+<main id="browser" class="browser">
+
+</main>
+
+<style>
+  #browser {
+    margin-top: 11vh;
+    margin-bottom: 11vh;
+    margin-inline: auto;
+    width: 95vw;
+    border: 0.3rem solid black;
+    min-height: 200vh;
+    background-color: #adff2f;
+    border-radius: 3vh;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Intro from "$lib/lp/Intro.svelte";
+  import Browser from "$lib/lp/Browser.svelte";
+</script>
+
+<Intro />
+<Browser />
